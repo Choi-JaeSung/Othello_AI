801dfa96d67ea1b4f69cb1f8e34886daff0794d5
skyler <skyler4784@gmail.com>
2022-05-19 04:32:53 +0900

    fourth commit complete othello.py, add Node.py

--- __main__.py
+++ __main__.py
@@ -1,10 +1,60 @@
+import random as rd
 from components import Board
+from othello import Othello
 
 board = Board()
+othello = Othello(board.matrix)
 
-print('verbose=True')
-print(board.visualize(verbose=True))
-print()
+turn = 1
+color = 0
 
-print('verbose=False')
-print(board.visualize(verbose=False))
+while True:
+    print(turn, "번째 턴")
+    print('verbose=True')
+    print(board.visualize(verbose=True))
+    print()
+    
+    if turn % 2 == 1:
+        print("흑의 차례")
+        color = 4
+        next = othello.search(color)
+    else:
+        print("백의 차례")
+        color = 3
+        next = othello.search(color)
+    
+    print()
+    print(next)
+    print()
+    
+    if next == []:
+        print("둘 곳이 없습니다.")
+        print("턴을 넘깁니다.")
+        print()
+    else:
+        while True:
+            # x, y = map(int, input("좌표를 입력하세요: ").split(','))
+            is_right = False
+            x, y = rd.choice(next)
+            for coord in next:
+                if coord[0] == x and coord[-1] == y:
+                    is_right = True
+            
+            if is_right == True:
+                break
+            else:
+                print("올바른 좌표를 입력해주세요!")
+                print()
+        
+        othello.put(x, y, color)
+        board.change_board(othello.get_board())
+        
+    if othello.is_game_over() == True:
+        print(board.visualize(verbose=True))
+        othello.victory()
+        break
+    else:
+        turn += 1
+
+# print('verbose=False')
+# print(board.visualize(verbose=False))
--- othello.py
+++ othello.py
@@ -1,57 +1,107 @@
+import numpy as np
+
 class Othello:
     
-    def __init__(self, matrix):
-        self.matrix = matrix
+    def __init__(self, board):
+        self.board = board
         
     # self.matirx 반환
-    def get_matrix(self):
-        return self.matrix
+    def get_board(self):
+        return self.board
     
     # 현재 유저가 둘 수 있는 좌표 반환
     def search(self, color):
-        coords = [] # 현재 유저가 돌을 둘 수 있는 좌표
+        targets = [] # 현재 유저가 돌을 둘 수 있는 좌표
         delta = ((0, 1), (1, 0), (-1, 0), (0, -1), (1, 1), (-1, 1), (-1, -1), (1, -1)) # 8방향
-        n = len(self.matrix)
+        n = len(self.board)
         
         for y in range(1, n):
             for x in range(1, n):
-                if self.matrix[x, y] == color:
+                if self.board[x, y] == color:
                     for d in delta:
-                        coord = [] # 다른 색깔의 돌
+                        coords = [] # 다른 색깔의 돌
                         dx, dy = d
                         nx, ny = x + dx, y + dy
                         
                         while True:
                             if nx < 1 or ny < 1 or nx > n-1 or ny > n-1: # 모서리 체크
-                                coord = []
+                                coords = []
                                 break
-                            if coord == [] and self.matrix[nx, ny] == 1: # 바로 다음 빈칸 체크
+                            if coords == [] and self.board[nx, ny] == 1: # 바로 다음 빈칸 체크
                                 break
-                            if self.matrix[nx, ny] == color: # 같은 색깔 돌 체크
-                                coord = []
+                            if self.board[nx, ny] == color: # 같은 색깔 돌 체크
+                                coords = []
                                 break
                             else:
-                                if self.matrix[nx, ny] == 1: # 다른 돌 옆이 빈칸일 때 coords에 추가
-                                    coord = []
-                                    coords.append([nx, ny])
+                                if self.board[nx, ny] == 1: # 다른 돌 옆이 빈칸일 때 targets에 추가
+                                    coords = []
+                                    targets.append([nx, ny])
+                                    break
                                 else:
-                                    coord.append((nx, ny)) # 다른 색깔 돌 좌표 coord에 추가
+                                    coords.append((nx, ny)) # 다른 색깔 돌 좌표 coord에 추가
                                 
                             nx, ny = nx + dx, ny + dy
+                            
+        targets = list(set(map(tuple, targets)))
                     
-        return coords
+        return targets
     
     # 현재 유저가 해당 위치에 돌을 둠
-    def put(self, coord, color):
-        x = coord[0]
-        y = coord[1]
+    def put(self, x, y, color):
+        delta = ((0, 1), (1, 0), (-1, 0), (0, -1), (1, 1), (-1, 1), (-1, -1), (1, -1)) # 8방향
+        n = len(self.board) # n x n board
         
-        self.matrix[x, y] = color
+        targets = [] # 뒤집을 돌 좌표
+        targets.append([x, y])
         
+        for d in delta:
+            dx, dy = d
+            nx, ny = x + dx, y + dy
+            
+            coords = [] # target 기준 8방향에 있는 다른 color의 돌
+            
+            while True:
+                if nx < 1 or ny < 1 or nx > n-1 or ny > n-1: # 모서리 체크
+                    coords = []
+                    break
+                elif self.board[nx, ny] == 0 or self.board[nx, ny] == 1:
+                    coords = []
+                    break
+                elif coords != [] and self.board[nx, ny] == color: # 도착점 체크
+                    for coord in coords:
+                        targets.append(coord)
+                    break
+                elif abs(self.board[nx, ny] - color) == 1:
+                    coords.append([nx, ny])
+                
+                nx, ny = nx + dx, ny + dy
+        
+        for target in targets:
+            self.board[target[0], target[-1]] = color
     
     # 겜 종료 여부 확인
-    def is_game_over(self, search):
-        if search == []:
+    def is_game_over(self):
+        result = np.where(self.board == 1)
+        if result[0].size == 0:
             return True
         else:
             False
+    
+    # 승자 결정
+    def victory(self):
+        board_1d = self.board.flatten()
+        sum_white = np.count_nonzero(board_1d == 3)
+        sum_black = np.count_nonzero(board_1d == 4)
+        
+        print("흑 : ", sum_black)
+        print("백 : ", sum_white)
+        print()
+        
+        if sum_white == sum_black:
+            print("비김")
+        elif sum_white > sum_black:
+            print("백 승")
+        else:
+            print("흑 승")
+        
+        
